387be75e857b119e7826009f7fe33e2ca9f5a4e2
HIJRANA <100138786+Hijrana19@users.noreply.github.com>
2022-03-07 03:48:15 -0800

    Add files via upload

---
+++ Biodata.py
@@ -0,0 +1,14 @@
+NIM = input ('''Teknik Informatika
+Universitas Sulawesi Barat
+Masukkan NIM:''')
+if NIM == 'D0221082' :
+	print ("Nama   : Hijrana")
+	print ("NIM    : D0221082")
+	print ('Prodi  : Teknik Informatika F')
+	print ("Alamat : Desa Padang")
+	print ("Alumni : SMAN 1 Campalagian")
+	print ("Misi   : Menjadi orang yang berguna")
+else:
+	print ("Anda bukan anak teknik")
+
+	
